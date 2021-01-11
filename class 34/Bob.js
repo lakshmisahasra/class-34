8a33ac24803f77b6ef942080ca33295c899ff7c2
lakshmisahasra <73932325+lakshmisahasra@users.noreply.github.com>
2021-01-11 20:00:47 +0530

    Add files via upload

---
+++ class 34/Bob.js
@@ -0,0 +1,27 @@
+class Bob {
+  constructor(x, y, width, height,angle) {
+    var options = {
+        
+        'frictionAir':0.05,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    ellipseMode(RADIUS);
+    strokeWeight(4);
+    stroke ("green");
+    fill(255);
+    ellipse(0, 0, this.width, this.height);
+    pop();
+  }
+};
